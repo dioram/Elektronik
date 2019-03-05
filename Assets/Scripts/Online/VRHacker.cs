c06f60dcde63bef1735549115b58dbe0fab2b1fd
Павел Андреевич Козин <kozinp@yandex.ru>
2019-03-05 16:28:55 +0300

    VRHacker so close

--- Assets/Scripts/Online/VRHacker.cs
+++ Assets/Scripts/Online/VRHacker.cs
@@ -1,16 +1,67 @@
-﻿using System;
+﻿using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using UnityEngine;
+using System.Threading;
+using VRTK;
 
 namespace Elektronik.Online
 {
-    public static class VRHacker
+    [RequireComponent(typeof(VRHackerUDPConnection))]
+    public class VRHacker : MonoBehaviour
     {
-        public static Transform HeadsetTransform()
+        private VRHackerUDPConnection m_connection;
+        private Pose m_lastPose;
+        private Thread m_dataListener;
+        private bool m_stop;
+
+        // Start is called before the first frame update
+        void Awake()
         {
-            throw new NotImplementedException();
+            m_connection = GetComponent<VRHackerUDPConnection>();
+        }
+
+        void Start()
+        {
+            m_lastPose = new Pose();
+            m_lastPose.rotation = Quaternion.identity;
+            m_lastPose.position = Vector3.zero;
+            m_stop = false;
+            m_dataListener = new Thread(Listen);
+            m_dataListener.IsBackground = true;
+            m_dataListener.Start();
+        }
+
+        void OnDestroy()
+        {
+            m_stop = true;
+        }
+
+        void Listen()
+        {
+            while (!m_stop)
+            {
+                Pose newPose;
+                if (m_connection.GetPose(out newPose))
+                {
+                    m_lastPose = newPose;
+                }
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            HackPose();
+        }
+
+        private void HackPose()
+        {
+            Transform headset = VRTK_DeviceFinder.DeviceTransform(VRTK_DeviceFinder.Devices.Headset);
+            if (headset != null)
+            {
+                headset.rotation = m_lastPose.rotation;
+                headset.position = m_lastPose.position;
+            }
         }
     }
 }
