ad07fc2f54ca6a33f0e68a4c9213332fad30f6b0
Павел Андреевич Козин <kozinp@yandex.ru>
2019-04-15 11:35:53 +0300

    Hacker fix

--- Assets/Scripts/Online/VRHacker.cs
+++ Assets/Scripts/Online/VRHacker.cs
@@ -2,25 +2,19 @@
 
 namespace Elektronik.Online
 {
-    class VRHacker_New : MonoBehaviour
+    class VRHacker : MonoBehaviour
     {
         public Transform helmet;
         public Transform headset;
 
-        private bool m_positionalTracking;
-        
-        
         private void OnEnable()
         {
-            m_positionalTracking = UnityEngine.XR.InputTracking.disablePositionalTracking;
-            UnityEngine.XR.InputTracking.disablePositionalTracking = true;
             Debug.Log("Hacker enabled");
             UnityEngine.XR.XRDevice.DisableAutoXRCameraTracking(headset.gameObject.GetComponent<Camera>(), true);
         }
 
         private void OnDisable()
         {
-            UnityEngine.XR.InputTracking.disablePositionalTracking = m_positionalTracking;;
             Debug.Log("Hacker disabled");
             UnityEngine.XR.XRDevice.DisableAutoXRCameraTracking(headset.gameObject.GetComponent<Camera>(), false);
         }
