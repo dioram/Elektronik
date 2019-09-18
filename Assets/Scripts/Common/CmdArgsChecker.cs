c3cdf78decd4daef5e8f8045255cedc69de8c1fd
Kozin Paul <kozinp@yandex.ru>
2019-09-18 09:44:38 +0300

    added logo, files with maps can be associated with Elektronik

---
+++ Assets/Scripts/Common/CmdArgsChecker.cs
@@ -0,0 +1,24 @@
+﻿using Elektronik.Common.UI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Elektronik.Common
+{
+    public class CmdArgsChecker : MonoBehaviour
+    {
+        public MainMenuButtons mainMenu;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            string[] args = Environment.GetCommandLineArgs();
+            if (args.Length > 1)
+            {
+                mainMenu.OnOfflineModeClick();
+            }
+        }
+    }
+}
