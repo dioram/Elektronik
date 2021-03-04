2e4bac7be3857109b4a47ef93788fa84c40e3944
Pavel Krasnov <krasnovpavel0@gmail.com>
2021-03-04 16:30:39 +0300

    CI pipeline changed

--- .github/build.bat
+++ .github/build.bat
@@ -1,2 +1,3 @@
 ﻿"C:\Program Files\Unity\Hub\Editor\2020.2.4f1\Editor\Unity.exe" -quit -accept-apiupdate -batchmode -serial %1 -username %2 -password %3 -projectPath .\ -buildWindows64Player .\Build\Elektronik.exe
-dotnet publish
+cd plugins
+dotnet publish Protobuf -o ../Build/Plugins/Protobuf/libraries
