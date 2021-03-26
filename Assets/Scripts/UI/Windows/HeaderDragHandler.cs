75b328ec6d5c632cbcf0088416a7ed4f8194ea52
Pavel Krasnov <krasnovpavel0@gmail.com>
2021-03-26 14:13:56 +0300

    Dragable windows

---
+++ Assets/Scripts/UI/Windows/HeaderDragHandler.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Elektronik.UI.Windows
+{
+    public class HeaderDragHandler : MonoBehaviour, IDragHandler
+    {
+        #region Unity events
+        
+        private void Start()
+        {
+            _window = transform.parent as RectTransform;
+            RectTransform testCanvas = _window;
+            while (_canvas == null && testCanvas != null)
+            {
+                _canvas = testCanvas.GetComponent<Canvas>();
+                testCanvas = testCanvas.parent as RectTransform;
+            }
+        }
+
+        #endregion
+
+        #region IDragHandler
+        
+        public void OnDrag(PointerEventData eventData)
+        {
+            var oldPos = _window.anchoredPosition;
+            _window.anchoredPosition += eventData.delta / _canvas.scaleFactor;
+            if (!IsInsideScreen()) _window.anchoredPosition = oldPos;
+        }
+
+        #endregion
+
+        #region Private
+
+        private RectTransform _window;
+        private Canvas _canvas;
+        
+        private bool IsInsideScreen()
+        {
+            var corners = new Vector3[4];
+            _window.GetWorldCorners(corners);
+            var rect = new Rect(0, 0, Screen.width, Screen.height);
+            return corners.All(corner => rect.Contains(corner));
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/Scripts/UI/Windows/WindowBase.cs
@@ -0,0 +1,25 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Elektronik.UI.Windows
+{
+    public class WindowBase : MonoBehaviour
+    {
+        #region Unity events
+
+        protected void Start()
+        {
+            Title = transform.Find("Header/Title").GetComponent<TMP_Text>();
+            Content = transform.Find("Content/Scroll View/Viewport/Content") as RectTransform;
+        }
+
+        #endregion
+
+        #region Protected
+
+        protected TMP_Text Title;
+        protected RectTransform Content;
+
+        #endregion
+    }
+}
