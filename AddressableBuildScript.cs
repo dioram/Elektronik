f411ddeb005e54467ab79a3008bd7ce808a46cee
Pavel Krasnov <krasnovpavel0@gmail.com>
2021-04-12 17:24:03 +0300

    Addressable CI build

---
+++ AddressableBuildScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+using UnityEditor.AddressableAssets.Settings;
+using UnityEditor.Build.Pipeline.Utilities;
+
+public static class AddressableBuildScript
+{
+    static void BuildPlayer()
+    {
+        AddressableAssetSettings.CleanPlayerContent();
+        BuildCache.PurgeCache(false);
+        AddressableAssetSettings.BuildPlayerContent();
+        BuildPipeline.BuildPlayer(new BuildPlayerOptions());
+    }
+}
