e7baed3f3e87e54b580776be66c25f3d1603b0e0
Pavel Krasnov <krasnovpavel0@gmail.com>
2021-04-29 12:03:47 +0300

    Link to github in main menu

---
+++ Assets/Scripts/UI/LinkOpener.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Elektronik.UI
+{
+    public class LinkOpener : MonoBehaviour
+    {
+        [SerializeField] private string GithubLink = "https://github.com/dioram/Elektronik-Tools-2.0";
+        
+        public void GoToGithub()
+        {
+            Application.OpenURL(GithubLink);
+        }
+    }
+}
