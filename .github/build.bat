8ce380a6d140c1569b17dadea820e81722e87efa
Pavel Krasnov <krasnovpavel0@gmail.com>
2021-05-11 11:38:54 +0300

    CI for plugins tests

--- .github/build.bat
+++ .github/build.bat
@@ -10,7 +10,7 @@ mkdir ..\\data
 move *.csv ..\\data
 for %%I in (..\\..\\..\\Elektronik_Data\\Managed\\*.*) do del %%~nxI
 cd ../../../../plugins
-move .\\Protobuf\\*.proto ..\\Build\\Plugins\\Protobuf
+copy .\\Protobuf\\*.proto ..\\Build\\Plugins\\Protobuf\\Data
 
 cd ROS2DDS
 cmake -G "Ninja" -DCMAKE_TOOLCHAIN_FILE=D:\\vcpkg\\scripts\\buildsystems\\vcpkg.cmake -DCMAKE_CXX_COMPILER="C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Tools/Llvm/x64/bin/clang-cl.exe" -DCMAKE_CXX_FLAGS=-m64 -DCMAKE_BUILD_TYPE=Release .
