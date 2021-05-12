72590118fc0b6d8726518a3e29bf36dbfe1a3b28
Pavel Krasnov <krasnovpavel0@gmail.com>
2021-05-12 18:43:23 +0300

    Updated release notes and testing script

--- .github/build.bat
+++ .github/build.bat
@@ -1,6 +1,6 @@
 ﻿"C:\Program Files\Unity\Hub\Editor\2020.2.4f1\Editor\Unity.exe" -quit -accept-apiupdate -batchmode -serial %1 -username %2 -password %3 -logFile .\Logs\pre_build.log -executeMethod Elektronik.Editor.PlayerBuildScript.BuildAddressables -projectPath .\
 "C:\Program Files\Unity\Hub\Editor\2020.2.4f1\Editor\Unity.exe" -quit -accept-apiupdate -batchmode -serial %1 -username %2 -password %3 -logFile .\Logs\build.log -projectPath .\ -buildWindows64Player .\Build\Elektronik.exe 
-"C:\Program Files\Unity\Hub\Editor\2020.2.4f1\Editor\Unity.exe" -quit -accept-apiupdate -batchmode -serial %1 -username %2 -password %3 -logFile .\Logs\tests.log -projectPath .\ -runEditorTests -editorTestsResultFile .\tests.xml
+"C:\Program Files\Unity\Hub\Editor\2020.2.4f1\Editor\Unity.exe" -quit -accept-apiupdate -batchmode -serial %1 -username %2 -password %3 -logFile .\Logs\tests.log -projectPath .\ -runTests -testResults .\tests.xml
 
 cd plugins
 dotnet publish ContextMenuSetter -o ../Build/Plugins/ContextMenuSetter
