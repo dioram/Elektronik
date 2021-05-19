ec0bf672f5c45d126ce457edd1350ce90e6029dc
Pavel Krasnov <krasnovpavel0@gmail.com>
2021-05-19 10:13:51 +0300

    Added failed test

--- Assets/Scripts/EditorTests/DataRecorderSubscription.cs
+++ Assets/Scripts/EditorTests/DataRecorderSubscription.cs
@@ -156,5 +156,11 @@ namespace Elektronik.EditorTests
             mockedRecorder.Verify(r => r.OnRemoved<SlamObservation>(It.IsAny<string>(),  It.IsAny<List<int>>()),
                                    Times.Once());
         }
+
+        [Test]
+        public void FailedTest()
+        {
+            Assert.Fail();
+        }
     }
 }
