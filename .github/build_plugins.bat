403e6599cf704691b3b0de138db38ae9868a0b0e
Pavel Krasnov <krasnovpavel0@gmail.com>
2021-08-05 15:10:34 +0300

    CI

--- .github/build_plugins.bat
+++ .github/build_plugins.bat
@@ -1,11 +1,12 @@
-﻿cd plugins
+﻿cd ./
+cd plugins
 dotnet publish ContextMenuSetter -o ../Build/Plugins/ContextMenuSetter
 dotnet publish Updater -o ../Build/Plugins/Updater
 
 dotnet publish Protobuf -o ../Build/Plugins/Protobuf/libraries
 cd ..\\Build\\Plugins\\Protobuf\\libraries
 mkdir ..\\data
-move *.csv ..\\data
+mv *.csv ..\\data
 for %%I in (..\\..\\..\\Elektronik_Data\\Managed\\*.*) do del %%~nxI
 cd ../../../../plugins
 copy .\\Protobuf\\*.proto ..\\Build\\Plugins\\Protobuf\\Data
@@ -13,6 +14,6 @@ copy .\\Protobuf\\*.proto ..\\Build\\Plugins\\Protobuf\\Data
 dotnet publish Ros -o ../Build/Plugins/Ros/libraries
 cd ..\\Build\\Plugins\\Ros\\libraries
 mkdir ..\\data
-move *.csv ..\\data
+mv *.csv ..\\data
 for %%I in (..\\..\\..\\Elektronik_Data\\Managed\\*.*) do del %%~nxI
 cd ../../../../plugins
