5570ad41ca9f56ecf668d98ebfde3e627d7636fb
Pavel Krasnov <krasnovpavel0@gmail.com>
2021-08-05 14:41:46 +0300

    CI

---
+++ .github/build_plugins.sh
@@ -0,0 +1,16 @@
+﻿cd plugins
+dotnet publish ContextMenuSetter -o ../build/Plugins/ContextMenuSetter
+dotnet publish Updater -o ../build/Plugins/Updater
+
+dotnet publish Protobuf -o ../build/Plugins/Protobuf/libraries
+cd ../build/Plugins/Protobuf
+mkdir ./data
+move ./libraries/*.csv ./data
+cd ../../../plugins
+cp ./Protobuf/*.proto ../build/Plugins/Protobuf/Data
+
+dotnet publish Ros -o ../build/Plugins/Ros/libraries
+cd ../build/Plugins/Ros/libraries
+mkdir ./data
+move ./libraries/*.csv ./data
+cd ../../../plugins
