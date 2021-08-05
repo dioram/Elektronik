fbb94e8624210e7a11011fc7a9563f285f76ed16
Pavel Krasnov <krasnovpavel0@gmail.com>
2021-08-05 16:17:02 +0300

    Update build_plugins.sh

--- .github/build_plugins.sh
+++ .github/build_plugins.sh
@@ -1,16 +1,14 @@
-﻿cd ./
+cd ./
 cd ./plugins
-dotnet publish ContextMenuSetter -o ../build/Plugins/ContextMenuSetter
-dotnet publish Updater -o ../build/Plugins/Updater
 
-dotnet publish Protobuf -o ../build/Plugins/Protobuf/libraries
+sudo dotnet publish Protobuf -o ../build/Plugins/Protobuf/libraries
 cd ../build/Plugins/Protobuf
 mkdir ./data
 mv ./libraries/*.csv ./data
 cd ../../../plugins
 cp ./Protobuf/*.proto ../build/Plugins/Protobuf/Data
 
-dotnet publish Ros -o ../build/Plugins/Ros/libraries
+sudo dotnet publish Ros -o ../build/Plugins/Ros/libraries
 cd ../build/Plugins/Ros/libraries
 mkdir ./data
 mv ./libraries/*.csv ./data
