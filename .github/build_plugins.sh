4068165829b3c3d1e5361ea29a8e48895b53b6f5
Pavel Krasnov <krasnovpavel0@gmail.com>
2021-08-06 10:37:08 +0300

    Update build_plugins.sh

--- .github/build_plugins.sh
+++ .github/build_plugins.sh
@@ -5,14 +5,14 @@ ls -la
 cd ../
 cd ./plugins
 
-sudo dotnet publish Protobuf -o ../build/Plugins/Protobuf/libraries
+dotnet publish Protobuf -o ../build/Plugins/Protobuf/libraries
 cd ../build/Plugins/Protobuf
 mkdir ./data
 mv ./libraries/*.csv ./data
 cd ../../../plugins
 cp ./Protobuf/*.proto ../build/Plugins/Protobuf/Data
 
-sudo dotnet publish Ros -o ../build/Plugins/Ros/libraries
+dotnet publish Ros -o ../build/Plugins/Ros/libraries
 cd ../build/Plugins/Ros/libraries
 mkdir ./data
 mv ./libraries/*.csv ./data
