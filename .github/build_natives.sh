530facc5b26e2310ef25372313a3dcdce736023a
Pavel Krasnov <krasnovpavel0@gmail.com>
2021-08-06 12:39:35 +0300

    Update build_natives.sh

--- .github/build_natives.sh
+++ .github/build_natives.sh
@@ -1,4 +1,3 @@
-cd &1
 cmake -DCMAKE_TOOLCHAIN_FILE=$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake -DCMAKE_CXX_FLAGS=-m64 -DCMAKE_BUILD_TYPE=Release $2 .
 cmake --build .
 cmake -DCMAKE_TOOLCHAIN_FILE=$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake -DCMAKE_CXX_FLAGS=-m64 -DCMAKE_BUILD_TYPE=Release $2 .
