964632929ff502f1794909c8d7c6c64c50da42b9
Pavel Krasnov <krasnovpavel0@gmail.com>
2021-09-22 15:26:36 +0300

    CI now uses workflow_run to make releases

---
+++ .github/update_version.ps1
@@ -0,0 +1,3 @@
+﻿$global:el=cat .\ProjectSettings\ProjectSettings.asset | Select-String -Pattern '(?:bundleVersion: )((\d*\.\d*\.\d*)(?:-rc\d*)?)' | % {$_.Matches.Groups[2].Value}
+cat .\ProjectSettings\ProjectSettings.asset | Foreach {$_ -replace 'bundleVersion: \d*\.\d*\.\d*(?:-rc\d*)?', "bundleVersion: $el"} | Set-Content ps.txt
+move -Force ps.txt .\ProjectSettings\ProjectSettings.asset
---
+++ .github/update_version.sh
@@ -0,0 +1,6 @@
+﻿#!/bin/bash
+export el=`cat ./ProjectSettings/ProjectSettings.asset | grep -oP '(?<=bundleVersion: )\d*.\d*.\d*(?=-rc\d*)?'`
+perl -pe 's/(?<=bundleVersion: )\d*.\d*.\d*(?:-rc\d*)?/'$el'/' < ./ProjectSettings/ProjectSettings.asset > psl.txt
+perl -pe 's/(?<=Version: )\d*.\d*.\d*(?:-rc\d*)?/'$el'/' < ./package/DEBIAN/control > control.txt
+cp -f psl.txt ./ProjectSettings/ProjectSettings.asset
+cp -f control.txt ./package/DEBIAN/control
